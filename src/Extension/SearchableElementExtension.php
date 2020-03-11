fbfa1a8a81337da5f1a9f50d83acaacd08896065
Daniel Dvoracek <daniel@somar.co.nz>
2020-03-11 16:28:05 +1300

    Workaround for cases of nested elements

--- src/Extension/SearchableElementExtension.php
+++ src/Extension/SearchableElementExtension.php
@@ -2,7 +2,9 @@
 
 namespace Somar\Search\Extension;
 
+use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\Versioned\Versioned;
 
 /**
  * Allow a Page to detect when Elemental content has changed
@@ -16,12 +18,10 @@ class SearchableElementExtension extends DataExtension
      */
     public function onBeforeWrite()
     {
-        $page = $this->owner->getPage();
         $element = $this->owner;
-
         // Workaround to detect if this Element has changes that need published
-        if (!$element->isLiveVersion() && $element->isModifiedOnDraft()) {
-            $page->putDocument();
+        if (!$this->owner->isLiveVersion() && $this->owner->isModifiedOnDraft()) {
+            $this->getParentPage($this->owner)->putDocument();
         }
     }
 
@@ -33,5 +33,25 @@ class SearchableElementExtension extends DataExtension
      */
     public function onBeforePublish()
     {
+    }
+
+    /**
+     * To fix incorrect behavior when nested elements
+     *
+     */
+    private function getParentPage($element)
+    {
+        // Change stage to draft in case of unpublished parent element
+        $originalStage = Versioned::get_stage();
+        Versioned::set_stage(Versioned::DRAFT);
+
+        $parent = $element->getPage();
+        while (!$parent instanceof SiteTree) {
+            $parent = $parent->getPage();
+        }
+
+        Versioned::set_stage($originalStage);
+
+        return $parent;
     }
 }
